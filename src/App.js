6769f65438f91cbed55c6d5d023a20dca80a2f73
backdfb <wva22lfr@gmail.com>
2024-03-20 19:30:02 +0900

    오류 해결

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { Container, Row, Col } from 'react-bootstrap';
-import ContactForm from './components/ContactForm';
-import ContactList from './components/ContactList';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Container, Row, Col } from "react-bootstrap";
+import ContactForm from "./components/ContactForm";
+import ContactList from "./components/ContactList";
+import { useSelector } from "react-redux";
 
 function App() {
+  const datas = useSelector((state) => state).contacts.contacts;
+  console.log(datas);
+
   return (
     <div className="App">
       <h1 className="title">연락처</h1>
       <Container>
-       <Row>
-        <Col>
+        <Row>
+          <Col>
             <ContactForm></ContactForm>
           </Col>
 
-         <Col>
-           <ContactList></ContactList>
-          </Col>
+          <Col></Col>
+          {datas?.map((data, i) => (
+            <div key={i}>
+              <li>{data.name}</li>
+              <li>{data.phoneNumber}</li>
+            </div>
+          ))}
+          <ContactList></ContactList>
         </Row>
-       
       </Container>
     </div>
   );
